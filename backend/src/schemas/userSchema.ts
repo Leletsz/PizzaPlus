4af12674ee92483e386a2ec344d40a2c7c59ca72
devlets <leandrovictor2011@gmail.com>
2026-03-13 19:15:14 -0300

    Initial commit

---
+++ backend/src/schemas/userSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+export const createUserSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ error: "o nome precisa ser um texto" })
+      .min(3, { error: "O nome precisa ter no minimo 3 letras" }),
+      email: z.email({error:"Precisa ser um email valido!"}),
+      password: z.string({error: "A senha é obrigatória"}).min(6,{error: "A senha deve ter no minimo 6 caracteres"})
+  }),
+});
---
+++ backend/src/services/user/CreateUserService.ts
@@ -0,0 +1,7 @@
+class CreateUserService {
+  async execute() {
+    return "usuario criado!";
+  }
+}
+
+export { CreateUserService };
